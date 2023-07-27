37e2e4263a97d9dc46928da200a74f95efe49796
Bunny <varun.verma2020@vitstudent.ac.in>
2023-07-27 07:15:25 +0530

    completed custom checkboxes

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const sliderValue = document.getElementById("sliderValue");
 slider.addEventListener("input", function() {
   sliderValue.textContent = this.value;
 });
+
+// Add event listeners to checkboxes to call the updateRectanglesColor function when they are checked/unchecked
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', updateRectanglesColor);
+});
